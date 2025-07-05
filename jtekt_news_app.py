1e7017a333def526ca825f65e5be3ea3b3e2a67e
Anjyu02 <58535886+Anjyu02@users.noreply.github.com>
2025-07-05 21:40:02 +0900

    Update jtekt_news_app.py

--- jtekt_news_app.py
+++ jtekt_news_app.py
@@ -154,7 +154,7 @@ if start_date > end_date:
 else:
     if st.button("✅ ニュースを抽出する"):
         with st.spinner("記事を抽出中です..."):
-            df = scrape_articles(start_date.year)  # ← 開始日を関数に渡す
+            df = scrape_articles(start_date.year, end_date)
             if df.empty:
                 st.warning("記事が見つかりませんでした。")
             else:
